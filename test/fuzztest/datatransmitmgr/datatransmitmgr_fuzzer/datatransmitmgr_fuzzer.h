cb9f5d6da0a60a3613206461c802ca45104bb65b
wangyongzhong2 <wangyongzhong2@huawei.com>
2022-11-21 15:07:52 +0800

    update fuzz test
    Signed-off-by: wangyongzhong2 <wangyongzhong2@huawei.com>

--- test/fuzztest/datatransmitmgr/datatransmitmgr_fuzzer/datatransmitmgr_fuzzer.h
+++ test/fuzztest/datatransmitmgr/datatransmitmgr_fuzzer/datatransmitmgr_fuzzer.h
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 
+#ifndef DATATRANSMITMGR_FUZZER_H
+#define DATATRANSMITMGR_FUZZER_H
+
 #include <cstdint>
 #include <unistd.h>
 #include <climits>
@@ -22,3 +25,4 @@
 
 #define FUZZ_PROJECT_NAME "datatransmitmgr_fuzzer"
 
+#endif
