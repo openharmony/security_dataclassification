6ae46f45a8a59962b9456e3171271b7860851fd3
jinduo <jinmengtong@huawei.com>
2023-09-22 03:31:47 +0000

    modify fuzz test
    Signed-off-by: jinduo <jinmengtong@huawei.com>

--- test/fuzztest/datatransmitmgr/datatransmitmgr_fuzzer/datatransmitmgr_fuzzer.cpp
+++ test/fuzztest/datatransmitmgr/datatransmitmgr_fuzzer/datatransmitmgr_fuzzer.cpp
@@ -15,15 +15,11 @@
 
 #include "datatransmitmgr_fuzzer.h"
 
-#include <stddef.h>
-#include <stdint.h>
+#include <cstddef>
+#include <cstdint>
 
 #include "securec.h"
 #include "dev_slinfo_mgr.h"
-#include "softbus_bus_center.h"
-#include "nativetoken_kit.h"
-#include "token_setproc.h"
-#include "accesstoken_kit.h"
 
 namespace OHOS {
     static void tmpCallback(DEVSLQueryParams *queryParams, int32_t result, uint32_t levelInfo)
@@ -31,75 +27,29 @@ namespace OHOS {
         return;
     }
 
-    static void GetLocalUdid(DEVSLQueryParams *queryParams)
-    {
-        const char *pkgName = "ohos.dslm";
-        NodeBasicInfo info;
-        (void)GetLocalNodeDeviceInfo(pkgName, &info);
-        if (GetNodeKeyInfo(pkgName, info.networkId, NODE_KEY_UDID, (uint8_t *)(queryParams->udid), UDID_BUF_LEN) != 0) {
-            return;
-        }
-        queryParams->udidLen = MAX_UDID_LENGTH;
-    }
-
     void FuzzDoDataTransmitMgr(const uint8_t* data, size_t size)
     {
-        if (data == nullptr) {
+        if (data == nullptr || size > MAX_UDID_LENGTH) {
             return;
         }
-        if (size < MAX_UDID_LENGTH) {
-            return;
-        }
+
         uint32_t levelInfo = 0;
         DEVSLQueryParams queryParams;
         (void)memset_s(&queryParams, sizeof(DEVSLQueryParams), 0, sizeof(DEVSLQueryParams));
         queryParams.udidLen = MAX_UDID_LENGTH;
         (void)memcpy_s(queryParams.udid, queryParams.udidLen, data, MAX_UDID_LENGTH);
 
-        int32_t ret = DATASL_OnStart();
-        if (ret != DEVSL_SUCCESS) {
-            return;
-        }
-        (void)DATASL_GetHighestSecLevelAsync(&queryParams, tmpCallback);
-        (void)DATASL_GetHighestSecLevelAsync(nullptr, tmpCallback);
-        (void)DATASL_GetHighestSecLevelAsync(&queryParams, nullptr);
-        (void)DATASL_GetHighestSecLevel(&queryParams, &levelInfo);
-        (void)DATASL_GetHighestSecLevel(nullptr, &levelInfo);
-        (void)DATASL_GetHighestSecLevel(&queryParams, nullptr);
-        GetLocalUdid(&queryParams);
+        (void)DATASL_OnStart();
         (void)DATASL_GetHighestSecLevelAsync(&queryParams, tmpCallback);
         (void)DATASL_GetHighestSecLevel(&queryParams, &levelInfo);
         DATASL_OnStop();
     }
 }
 
-static void NativeTokenGetFUZZ()
-{
-    uint64_t tokenId;
-    const char **perms = new const char *[1];
-    perms[0] = "ohos.permission.DISTRIBUTED_DATASYNC";
-    NativeTokenInfoParams infoInstance = {
-        .acls = nullptr,
-        .dcaps = nullptr,
-        .aclsNum = 0,
-        .dcapsNum = 0,
-        .permsNum = 1,
-        .perms = perms,
-        .aplStr = "system_basic",
-    };
-
-    infoInstance.processName = "DataTransmitMgrFuzzTest";
-    tokenId = GetAccessTokenId(&infoInstance);
-    SetSelfTokenID(tokenId);
-    OHOS::Security::AccessToken::AccessTokenKit::ReloadNativeTokenInfo();
-    delete[] perms;
-}
-
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
-    NativeTokenGetFUZZ();
     OHOS::FuzzDoDataTransmitMgr(data, size);
     return 0;
 }
