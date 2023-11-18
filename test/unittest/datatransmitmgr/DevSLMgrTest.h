7a73d8c6c43214ada1f268e66f097ffc90d6b377
gaohongliang410 <gaohongliang410@huawei.com>
2023-11-18 06:54:04 +0000

    modify tdd test
    Signed-off-by: gaohongliang410 <gaohongliang410@huawei.com>

--- test/unittest/datatransmitmgr/DevSLMgrTest.h
+++ test/unittest/datatransmitmgr/DevSLMgrTest.h
@@ -16,4 +16,25 @@
 #ifndef DATASECURITYLEVEL_TEST_H
 #define DATASECURITYLEVEL_TEST_H
 
+#include <gtest/gtest.h>
+
+namespace OHOS {
+namespace Security {
+namespace DevSLMgrTest {
+using namespace testing::ext;
+class DevSLMgrTest : public testing::Test {
+public:
+    DevSLMgrTest();
+    ~DevSLMgrTest();
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+private:
+    static bool isEnforcing_;
+};
+}
+}
+}
+
 #endif
